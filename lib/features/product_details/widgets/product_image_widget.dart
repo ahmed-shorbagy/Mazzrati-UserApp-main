5bf1950381529252a0eef4eb515aac08bb11ed87
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2025-03-29 17:47:21 +0200

    Refactor product image widget for improved readability
    Reformatted the `ProductImageWidget` code to enhance readability and maintain consistency. The changes include:
    - Adjusted indentation and line breaks for better code structure.
    - Ensured proper alignment of nested widgets and parameters.
    - No functional changes were made; the refactor focuses solely on code style.

--- lib/features/product_details/widgets/product_image_widget.dart
+++ lib/features/product_details/widgets/product_image_widget.dart
@@ -184,36 +184,39 @@ class ProductImageWidget extends StatelessWidget {
                                                   Dimensions.paddingSizeSmall,
                                             ),
                                             InkWell(
-                                                onTap: () {
-                                                  if (productController
-                                                          .sharableLink !=
-                                                      null) {
-                                                    Share.share(
-                                                        productController
-                                                            .sharableLink!);
-                                                  }
-                                                },
-                                                child: Card(
-                                                    elevation: 2,
-                                                    shape: RoundedRectangleBorder(
-                                                        borderRadius:
-                                                            BorderRadius.circular(
-                                                                50)),
-                                                    child: Container(
-                                                        width: 40,
-                                                        height: 40,
-                                                        decoration: BoxDecoration(
-                                                            color: Theme.of(context)
-                                                                .cardColor,
-                                                            shape: BoxShape
-                                                                .circle),
-                                                        child: Padding(
-                                                            padding: const EdgeInsets.all(
-                                                                Dimensions
-                                                                    .paddingSizeSmall),
-                                                            child: Image.asset(
-                                                                Images.share,
-                                                                color: Theme.of(context).primaryColor)))))
+                                              onTap: () {
+                                                if (productController
+                                                        .sharableLink !=
+                                                    null) {
+                                                  Share.share(productController
+                                                      .sharableLink!);
+                                                }
+                                              },
+                                              child: Card(
+                                                elevation: 2,
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            50)),
+                                                child: Container(
+                                                  width: 40,
+                                                  height: 40,
+                                                  decoration: BoxDecoration(
+                                                      color: Theme.of(context)
+                                                          .cardColor,
+                                                      shape: BoxShape.circle),
+                                                  child: Padding(
+                                                    padding: const EdgeInsets
+                                                        .all(Dimensions
+                                                            .paddingSizeSmall),
+                                                    child: Image.asset(
+                                                        Images.share,
+                                                        color: Theme.of(context)
+                                                            .primaryColor),
+                                                  ),
+                                                ),
+                                              ),
+                                            )
                                           ])),
                                       Positioned(
                                           top: 10,
